6863bb97a4c641c3a2c47f2fe2000fad85546190
Oracle-PC <oracle@kahatex.com>
2024-02-24 12:52:43 +0700

    push dulu sementara retrieve

---
+++ FormBatchScheduler.vb
@@ -0,0 +1,9 @@
+﻿Public Class FormBatchScheduler
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Guna2Button3_Click(sender As Object, e As EventArgs) Handles Guna2Button3.Click
+        FormRecipeSelector.Show()
+    End Sub
+End Class
