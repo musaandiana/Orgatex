657d20134284e84fe5e87efe4c121c757be0066c
CPB F5 PUSAT <CPB F5 PUSAT@F5PUSAT>
2024-02-26 10:15:38 +0700

    dikit dikit push

--- FormBatchList.Designer.vb
+++ FormBatchList.Designer.vb
@@ -36,6 +36,7 @@ Partial Class FormBatchList
         'Guna2ComboBox1
         '
         Me.Guna2ComboBox1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ComboBox1.BorderRadius = 6
         Me.Guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
         Me.Guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.Guna2ComboBox1.FocusedColor = System.Drawing.Color.Empty
@@ -47,17 +48,17 @@ Partial Class FormBatchList
         Me.Guna2ComboBox1.ItemHeight = 30
         Me.Guna2ComboBox1.Items.AddRange(New Object() {"CPB", "PDY"})
         Me.Guna2ComboBox1.ItemsAppearance.Parent = Me.Guna2ComboBox1
-        Me.Guna2ComboBox1.Location = New System.Drawing.Point(304, 15)
+        Me.Guna2ComboBox1.Location = New System.Drawing.Point(98, 57)
         Me.Guna2ComboBox1.Name = "Guna2ComboBox1"
         Me.Guna2ComboBox1.ShadowDecoration.Parent = Me.Guna2ComboBox1
-        Me.Guna2ComboBox1.Size = New System.Drawing.Size(101, 36)
+        Me.Guna2ComboBox1.Size = New System.Drawing.Size(227, 36)
         Me.Guna2ComboBox1.TabIndex = 24
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(22, 25)
+        Me.Label1.Location = New System.Drawing.Point(22, 41)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(60, 20)
         Me.Label1.TabIndex = 22
@@ -65,6 +66,7 @@ Partial Class FormBatchList
         '
         'txt_recipe
         '
+        Me.txt_recipe.BorderRadius = 6
         Me.txt_recipe.Cursor = System.Windows.Forms.Cursors.IBeam
         Me.txt_recipe.DefaultText = ""
         Me.txt_recipe.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
@@ -82,73 +84,77 @@ Partial Class FormBatchList
         Me.txt_recipe.PlaceholderText = ""
         Me.txt_recipe.SelectedText = ""
         Me.txt_recipe.ShadowDecoration.Parent = Me.txt_recipe
-        Me.txt_recipe.Size = New System.Drawing.Size(200, 36)
+        Me.txt_recipe.Size = New System.Drawing.Size(227, 36)
         Me.txt_recipe.TabIndex = 21
         '
         'dgbuyer
         '
         Me.dgbuyer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgbuyer.Location = New System.Drawing.Point(26, 69)
+        Me.dgbuyer.Location = New System.Drawing.Point(26, 99)
         Me.dgbuyer.Name = "dgbuyer"
         Me.dgbuyer.ReadOnly = True
-        Me.dgbuyer.Size = New System.Drawing.Size(529, 609)
+        Me.dgbuyer.Size = New System.Drawing.Size(529, 579)
         Me.dgbuyer.TabIndex = 25
         '
         'Guna2Button1
         '
+        Me.Guna2Button1.BorderRadius = 6
         Me.Guna2Button1.CheckedState.Parent = Me.Guna2Button1
         Me.Guna2Button1.CustomImages.Parent = Me.Guna2Button1
         Me.Guna2Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.Guna2Button1.ForeColor = System.Drawing.Color.White
         Me.Guna2Button1.HoverState.Parent = Me.Guna2Button1
-        Me.Guna2Button1.Location = New System.Drawing.Point(453, 15)
+        Me.Guna2Button1.Location = New System.Drawing.Point(422, 15)
         Me.Guna2Button1.Name = "Guna2Button1"
         Me.Guna2Button1.ShadowDecoration.Parent = Me.Guna2Button1
-        Me.Guna2Button1.Size = New System.Drawing.Size(60, 14)
+        Me.Guna2Button1.Size = New System.Drawing.Size(76, 36)
         Me.Guna2Button1.TabIndex = 26
         Me.Guna2Button1.Text = "Edit"
         '
         'Guna2Button2
         '
+        Me.Guna2Button2.BorderRadius = 6
         Me.Guna2Button2.CheckedState.Parent = Me.Guna2Button2
         Me.Guna2Button2.CustomImages.Parent = Me.Guna2Button2
         Me.Guna2Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.Guna2Button2.ForeColor = System.Drawing.Color.White
         Me.Guna2Button2.HoverState.Parent = Me.Guna2Button2
-        Me.Guna2Button2.Location = New System.Drawing.Point(453, 35)
+        Me.Guna2Button2.Location = New System.Drawing.Point(422, 57)
         Me.Guna2Button2.Name = "Guna2Button2"
         Me.Guna2Button2.ShadowDecoration.Parent = Me.Guna2Button2
-        Me.Guna2Button2.Size = New System.Drawing.Size(60, 14)
+        Me.Guna2Button2.Size = New System.Drawing.Size(76, 36)
         Me.Guna2Button2.TabIndex = 27
         Me.Guna2Button2.Text = "Print"
         '
         'Guna2Button3
         '
+        Me.Guna2Button3.BorderRadius = 6
         Me.Guna2Button3.CheckedState.Parent = Me.Guna2Button3
         Me.Guna2Button3.CustomImages.Parent = Me.Guna2Button3
         Me.Guna2Button3.FillColor = System.Drawing.Color.Maroon
         Me.Guna2Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.Guna2Button3.ForeColor = System.Drawing.Color.White
         Me.Guna2Button3.HoverState.Parent = Me.Guna2Button3
-        Me.Guna2Button3.Location = New System.Drawing.Point(519, 15)
+        Me.Guna2Button3.Location = New System.Drawing.Point(504, 15)
         Me.Guna2Button3.Name = "Guna2Button3"
         Me.Guna2Button3.ShadowDecoration.Parent = Me.Guna2Button3
-        Me.Guna2Button3.Size = New System.Drawing.Size(36, 36)
+        Me.Guna2Button3.Size = New System.Drawing.Size(52, 78)
         Me.Guna2Button3.TabIndex = 28
-        Me.Guna2Button3.Text = "Del."
+        Me.Guna2Button3.Text = "Delete"
         '
         'Guna2Button4
         '
+        Me.Guna2Button4.BorderRadius = 6
         Me.Guna2Button4.CheckedState.Parent = Me.Guna2Button4
         Me.Guna2Button4.CustomImages.Parent = Me.Guna2Button4
         Me.Guna2Button4.FillColor = System.Drawing.SystemColors.ActiveCaption
         Me.Guna2Button4.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.Guna2Button4.ForeColor = System.Drawing.Color.White
         Me.Guna2Button4.HoverState.Parent = Me.Guna2Button4
-        Me.Guna2Button4.Location = New System.Drawing.Point(411, 15)
+        Me.Guna2Button4.Location = New System.Drawing.Point(331, 15)
         Me.Guna2Button4.Name = "Guna2Button4"
         Me.Guna2Button4.ShadowDecoration.Parent = Me.Guna2Button4
-        Me.Guna2Button4.Size = New System.Drawing.Size(36, 36)
+        Me.Guna2Button4.Size = New System.Drawing.Size(85, 78)
         Me.Guna2Button4.TabIndex = 29
         Me.Guna2Button4.Text = "New"
         '
@@ -165,6 +171,7 @@ Partial Class FormBatchList
         Me.Controls.Add(Me.Guna2ComboBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txt_recipe)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "FormBatchList"
         Me.Text = "FormBatchList"
         CType(Me.dgbuyer, System.ComponentModel.ISupportInitialize).EndInit()
